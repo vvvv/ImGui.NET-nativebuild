49b279fef53d6d186cf8c1564bbcd1c627816d5f
Elias Holzer <elias@vvvv.org>
2025-10-26 18:45:51 +0100

    Use cmake defaults

--- build-native.cmd
+++ build-native.cmd
@@ -5,12 +5,11 @@ set CIMGUI_ROOT=%~dp0cimgui
 set BUILD_CONFIG=Debug
 set BUILD_ARCH=x64
 set BUILD_CMAKE_GENERATOR_PLATFORM=x64
-set MSVC_RUNTIME="MultiThreadedDebug"
 
 :ArgLoop
 if [%1] == [] goto Build
-if /i [%1] == [Release] (set BUILD_CONFIG=Release && set MSVC_RUNTIME=MultiThreaded && shift & goto ArgLoop)
-if /i [%1] == [Debug] (set BUILD_CONFIG=Debug && set MSVC_RUNTIME=MultiThreadedDebug && shift & goto ArgLoop)
+if /i [%1] == [Release] (set BUILD_CONFIG=Release && shift & goto ArgLoop)
+if /i [%1] == [Debug] (set BUILD_CONFIG=Debug && shift & goto ArgLoop)
 if /i [%1] == [x64] (set BUILD_ARCH=x64 && shift & goto ArgLoop)
 if /i [%1] == [ARM64] (set BUILD_ARCH=ARM64 && set BUILD_CMAKE_GENERATOR_PLATFORM=ARM64 && shift & goto ArgLoop)
 if /i [%1] == [ARM] (set BUILD_ARCH=ARM && set BUILD_CMAKE_GENERATOR_PLATFORM=ARM && shift & goto ArgLoop)
@@ -25,7 +24,7 @@ If NOT exist ".\build\%BUILD_ARCH%" (
   mkdir build\%BUILD_ARCH%
 )
 pushd build\%BUILD_ARCH%
-cmake -DCMAKE_GENERATOR_PLATFORM=%BUILD_CMAKE_GENERATOR_PLATFORM% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME% ..\..
+cmake -DCMAKE_GENERATOR_PLATFORM=%BUILD_CMAKE_GENERATOR_PLATFORM% ..\..
 
 echo Calling cmake --build . --config %BUILD_CONFIG%
 cmake --build . --config %BUILD_CONFIG%
